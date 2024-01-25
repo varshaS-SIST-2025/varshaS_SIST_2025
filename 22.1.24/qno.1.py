fa6e7a35d878d0cf892fbcc9d3f79730adb51c2c
varshaS-SIST-2025 <157302987+varshaS-SIST-2025@users.noreply.github.com>
2024-01-25 19:32:28 +0530

    Create qno.1.py
    reverse a number

---
+++ 22.1.24/qno.1.py
@@ -0,0 +1,17 @@
+  REVERSE A NUMBER
+
+LOGIC 1
+
+n=int(input())
+print(str(n)[::-1])
+
+LOGIC 2
+
+n=int(input())
+rev=0
+while(n>0):
+    dig=n%10
+    rev=rev*10+dig
+    n//=10
+    
+print(rev)
